f7c95b6cdea30971749cd73853416d9b81241864
rodneyrick <rodneyrick@gmail.com>
2012-07-12 10:56:12 -0300

    Acréscimo do campo de email no usuário.

--- Coruja_GraoPara/JavaSource/login/User.java
+++ Coruja_GraoPara/JavaSource/login/User.java
@@ -19,7 +19,10 @@ public class User {
 	private String username;
 	@NotNull
 	private String password;
+	@NotNull
 	private String name;
+	@NotNull
+	private String email;
 	@ManyToOne
 	@JoinColumn(name="profile", nullable=false)
 	private Profile userProfile;
@@ -35,6 +38,9 @@ public class User {
 	
 	public String getName() {return name;}
 	public void setName(String name) {this.name = name;}
+	
+	public String getEmail() {return email;}
+	public void setEmail(String email) {this.email = email;}
 
 	public Profile getUserProfile() {return userProfile;}
 	public void setUserProfile(Profile userProfile) {this.userProfile = userProfile;}
