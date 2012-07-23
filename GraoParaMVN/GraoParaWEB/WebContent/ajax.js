6686911615c3f3211cbe162d56dba3cc7cec2991
matheusferreira <mts.seven@gmail.com>
2012-07-23 09:28:19 -0300

    Estrutura do projeto Web modificado para Web Dynamic (Project Facets) e AJAX rodando certo no Mozilla e no Chrome

---
+++ GraoParaMVN/GraoParaWEB/WebContent/ajax.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+function GetXMLHttp() {
+	if(navigator.appName == "Microsoft Internet Explorer") {
+		xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	else {
+		xmlHttp = new XMLHttpRequest();
+	}
+		return xmlHttp;
+	}
+ 
+	var xmlRequest = GetXMLHttp();
