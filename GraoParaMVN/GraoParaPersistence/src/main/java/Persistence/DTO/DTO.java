1ea88582d2906fd5e4d088c3e4df78aecbea32f9
Daniel Gracia <hueho@sidad-MS-7680>
2012-07-24 12:03:34 -0300

    Monte de coisa para fazer funções de atualização funcionarem, e começando o mais novo pacote de entityUpdater

--- GraoParaMVN/GraoParaPersistence/src/main/java/Persistence/DTO/DTO.java
+++
@@ -1,9 +0,0 @@
-package Persistence.DTO;
-
-/**
- * Interface vazia para type safe. <br/>
- * Todos os DTOs devem implementar essa interface.
- */
-public interface DTO {
-
-}
--- GraoParaMVN/GraoParaPersistence/src/main/java/Persistence/Model/Entidade.java
+++
@@ -1,7 +0,0 @@
-package Persistence.Model;
-
-/**
- * Interface vazia para type safe. <br/>
- * Todas as entidades devem implementar essa interface.
- */
-public interface Entidade {}
