c6a3dd27e31428e72a96a56e492396252a128d19
Vitor Kawai Sala <vitorsala@gmail.com>
2012-07-24 10:07:49 -0300

    Upando as interfaces que faltaram

---
+++ GraoParaMVN/GraoParaPersistence/src/main/java/Persistence/DTO/DTO.java
@@ -0,0 +1,9 @@
+package Persistence.DTO;
+
+/**
+ * Interface vazia para type safe. <br/>
+ * Todos os DTOs devem implementar essa interface.
+ */
+public interface DTO {
+
+}
---
+++ GraoParaMVN/GraoParaPersistence/src/main/java/Persistence/Model/Entidade.java
@@ -0,0 +1,7 @@
+package Persistence.Model;
+
+/**
+ * Interface vazia para type safe. <br/>
+ * Todas as entidades devem implementar essa interface.
+ */
+public interface Entidade {}
