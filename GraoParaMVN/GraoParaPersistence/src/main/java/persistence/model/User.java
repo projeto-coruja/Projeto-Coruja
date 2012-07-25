1b73795972f13520f207ef2da1f5cd36f2dfe2f1
Daniel Gracia <hueho@sidad-MS-7680>
2012-07-25 12:29:01 -0300

    Mudando modelo User para não chamar mais nenhuma função de Password.

--- GraoParaMVN/GraoParaPersistence/src/main/java/persistence/model/User.java
+++ GraoParaMVN/GraoParaPersistence/src/main/java/persistence/model/User.java
@@ -14,12 +14,9 @@ import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.NotEmpty;
 
-import persistence.utility.Password;
-
-
 @Entity
 @Table(name = "tbl_user")
-public class User implements Serializable,Entidade {
+public class User implements Serializable, Entidade {
 
 	private static final long serialVersionUID = 1115684572959901460L;
 
@@ -29,7 +26,7 @@ public class User implements Serializable,Entidade {
 
 	@NotNull
 	@NotEmpty
-	@Column(unique=true)
+	@Column(unique = true)
 	private String email;
 
 	@NotNull
@@ -42,29 +39,59 @@ public class User implements Serializable,Entidade {
 
 	@NotNull
 	@NotEmpty
-	@Column(name="data_inclusao")
+	@Column(name = "data_inclusao")
 	private Date dataInclusao;
 
 	@ManyToOne
 	@JoinColumn(name = "profile", nullable = false)
 	private Profile userProfile;
 
-	public Long getId() {	return id;	}
-	public void setId(Long id) {	this.id = id;	}
-	
-	public String getEmail() {	return email;	}
-	public void setEmail(String email) {	this.email = email;	}
-	
-	public String getPassword() {	return Password.getHash(password);	}
-	public void setPassword(String password) {	this.password = Password.getHash(password);	}
+	public Long getId() {
+		return id;
+	}
 
-	public String getName() {	return name;	}
-	public void setName(String name) {	this.name = name;	}
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-	public Date getDataInclusao() {	return dataInclusao;}
-	public void setDataInclusao(Date dataInclusao) {this.dataInclusao = dataInclusao;}
-	
-	public Profile getUserProfile() {	return userProfile;	}
-	public void setUserProfile(Profile userProfile) {	this.userProfile = userProfile;	}
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getDataInclusao() {
+		return dataInclusao;
+	}
+
+	public void setDataInclusao(Date dataInclusao) {
+		this.dataInclusao = dataInclusao;
+	}
+
+	public Profile getUserProfile() {
+		return userProfile;
+	}
+
+	public void setUserProfile(Profile userProfile) {
+		this.userProfile = userProfile;
+	}
 
 }
