900f660b002f7065558ba466c04031a5c5f6d977
Daniel Gracia <hueho@sidad-MS-7680>
2012-08-02 11:58:50 -0300

    Consertando

---
+++ GraoParaMVN/GraoParaWEB/src/main/java/test/LoginServlet.java
@@ -0,0 +1,34 @@
+package test;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Login")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			String user = request.getParameter("login");
+			String senha = request.getParameter("senha");
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+
+}
