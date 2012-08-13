b0cfc3b2af9c5bc12e7100a883de0b0d103d5614
rick <rick@sidad>
2012-08-13 10:16:41 -0300

    Javascript para controle do cookie

---
+++ GraoPara/WebContent/javascript/cookie.js
@@ -0,0 +1,19 @@
+function getCookie(c_name){
+	var i,x,y,ARRcookies=document.cookie.split(";");
+	for (i=0;i<ARRcookies.length;i++)
+	{
+		x=ARRcookies[i].substr(0,ARRcookies[i].indexOf("="));
+		y=ARRcookies[i].substr(ARRcookies[i].indexOf("=")+1);
+		x=x.replace(/^\s+|\s+$/g,"");
+		if (x==c_name) {
+			return unescape(y);
+		}
+	}
+}
+
+function checkCookie() {
+	var username=getCookie("email_graopara");
+	if (username!=null && username!="")	{
+		alert("Welcome again " + username);
+	}
+}
