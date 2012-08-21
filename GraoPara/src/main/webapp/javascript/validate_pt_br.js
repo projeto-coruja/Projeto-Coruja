daabafbbdca68f0fe9d41d3fefce2df34de62933
angela <angela@angela-MS-7680>
2012-08-21 10:52:40 -0300

    Atualização da na página de pesquisa que estava com caminho errado e vadidação dos campos do arquivo cadstro de documentos

--- GraoPara/src/main/webapp/javascript/validate_pt_br.js
+++ GraoPara/src/main/webapp/javascript/validate_pt_br.js
@@ -2,14 +2,51 @@ $(document).ready(function() {
 	// validate signup form on keyup and submit
 	var validator = $("#signupform").validate({
 		rules: {
+			/* ----------------------------------
+			 *  Cadastro de Documentos
+			 * ---------------------------------- */
+			identificacao: "required",
+			codigo: {
+				required: true,
+				minlength: 3,				
+			},
+			titulo: {
+				required: true,
+				minlength: 5,				
+			},
+			numero: {
+				required: true,
+				minlength: 3
+			},
+			tipo_num: required,
+			autor: {
+				required: true,
+				minlength: 5,				
+			},
+			destinatario: {
+				required: true,
+				minlength: 5,				
+			},
+			local: {
+				required: true,
+				minlength: 5,				
+			},
+			data:{
+				required: true,
+				date: true
+			},
+			tipoDoc: required,
+			resumo: required,
+			chave1: {
+				required: true,
+				minlength: 3
+			},
+			/* ----------------------------------
+			 *  Cadastro de Usuários
+			 * ---------------------------------- */
 			nome: {
 				required: true,
 				minlength: 5
-			},
-			lastname: "required",
-			username: {
-				required: true,
-				minlength: 5,				
 			},
 			senha: {
 				required: true,
@@ -24,36 +61,68 @@ $(document).ready(function() {
 				required: true,
 				email: true,
 			},
-			dateformat: "required",
-			terms: "required"
+			
 		},
 		messages: {
-			nome: {
-				required: "Digite seu nome completo",
-				minlength: jQuery.format("Digite no mínimo {0} caracteres")
+			/* ----------------------------------
+			 *  Cadastro de Documentos
+			 * ---------------------------------- */
+			identificacao: "Selecione a identificação.",
+			tipo_num: "Selecione o tipo de número.",
+			codigo: {
+				required: "Digite o código do documento.",
+				minlength: jQuery.format("Digite no mínimo {0} caracteres.")
 			},
-			lastname: "Enter your lastname",
-			username: {
-				required: "Enter a username",
-				minlength: jQuery.format("Digite no mínimo {0} caracteres"),
-				remote: jQuery.format("{0} is already in use")
+			titulo: {
+				required: "Digite o título do documento.",
+				minlength: jQuery.format("Digite no mínimo {0} caracteres.")
+			},			
+			numero: {
+				required: "Digite o número APEP ou Sequencial.",
+				minlength: jQuery.format("Digite no mínimo {0} caracteres.")
+			},
+			autor: {
+				required: "Digite o autor do documento.",
+				minlength: jQuery.format("Digite no mínimo {0} caracteres.")
+			},
+			destinatario: {
+				required: "Digite o destinatário do documento.",
+				minlength: jQuery.format("Digite no mínimo {0} caracteres.")
+			},
+			local: {
+				required: "Digite o local do documento.",
+				minlength: jQuery.format("Digite no mínimo {0} caracteres.")
+			},
+			data: {
+				required: "Entre com uma data válida.",
+				date: "Entre com uma data válida."					
+			},
+			tipoDoc: "Selecione o tipo de Documento.",
+			resumo: "Digite o resumo do Documento",
+			chave1: {
+				required: "Informe a primeira palavra chave.",
+				minlength: jQuery.format("Digite no mínimo {0} caracteres.")
+			},
+			/* ----------------------------------
+			 *  Cadastro de Usuários
+			 * ---------------------------------- */
+			nome: {
+				required: "Digite seu nome completo.",
+				minlength: jQuery.format("Digite no mínimo {0} caracteres.")
 			},
 			senha: {
 				required: "Digite sua senha",
-				minlength: jQuery.format("Digite no mínimo {0} caracteres")
+				minlength: jQuery.format("Digite no mínimo {0} caracteres.")
 			},
 			confsenha: {
 				required: "Confirme sua senha",
-				minlength: jQuery.format("Digite no mínimo {0} caracteres"),
-				equalTo: "Digite uma senha igual a anterior"
+				minlength: jQuery.format("Digite no mínimo {0} caracteres."),
+				equalTo: "Digite uma senha igual a anterior."
 			},
 			email: {
-				required: "Entre com um email válido",
-				minlength: "Entre com um email válido",
-				remote: jQuery.format("{0} is already in use")
-			},
-			dateformat: "Choose your preferred dateformat",
-			terms: " "
+				required: "Entre com um email válido.",
+				email: "Entre com um email válido."
+			}
 		},
 		// the errorPlacement has to take the table layout into account
 		errorPlacement: function(error, element) {
@@ -66,7 +135,7 @@ $(document).ready(function() {
 		},
 		// specifying a submitHandler prevents the default submit, good for the demo
 		submitHandler: function() {
-			alert("submitted!");
+			alert("Cadastro Realizado com Sucesso!");
 		},
 		// set this class to error-labels to indicate valid fields
 		success: function(label) {
