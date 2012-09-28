d5f3193f1e2308ce7595fb08c5751800cc8f0155
rodneyrick <rodneyrick@gmail.com>
2012-09-28 15:31:03 -0300

    InitServlet e WebUtility começando a alterar.

---
+++ GraoPara-BD2/src/main/java/webview/servlet/init/InitServlet.java
@@ -0,0 +1,209 @@
+package webview.servlet.init;
+
+import java.io.File;
+import java.util.Date;
+import java.util.List;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.PropertyConfigurator;
+
+import persistence.PersistenceAccess;
+import persistence.dto.DTO;
+import persistence.dto.Profile;
+import persistence.dto.TemaPalavraChave;
+import persistence.dto.TipoDocumento;
+import persistence.dto.UserAccount;
+import webview.servlet.util.WebUtility;
+
+import business.EJB.util.EJBUtility;
+
+/**
+ * Servlet implementation class InitServlet
+ */
+@WebServlet("/InitServlet")
+public class InitServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private static Log log = LogFactory.getLog(InitServlet.class);
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public InitServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+		initLogger(config);
+
+		log.info("Iniciando sistema Grão-Para...");
+		PersistenceAccess pa = new PersistenceAccess();
+		String[] profiles_names = {"default", "user", "admin"};
+
+		List<DTO> profile;
+		for (String p : profiles_names) {
+			profile = pa.findEntity("from ProfileMO where profile = '" + p + "'");
+			if(profile == null) {
+				log.info("Profile '" + p + "' não encontrado, recriando..." );
+				if(p.equals("default")) {
+					pa.saveEntity(WebUtility.default_profile);
+				}
+				else if(p.equals("user")) {
+					pa.saveEntity(WebUtility.user_profile);
+				}
+				else if(p.equals("admin")) {
+					pa.saveEntity(WebUtility.admin_profile);
+				}
+			}
+			else if(profile.size() > 1) {
+				log.error("Profile '" + p + "' repetido no banco de dados, verificar imediatamente.");
+			}
+		}
+
+		//TODO: MUDAR SENHA
+		List<DTO> user = pa.findEntity("from UserMO where email = 'admin@graopara.com'");
+		if(user == null)
+		{
+			log.info("Criando usuário de admin...");
+			pa.saveEntity(
+					new UserAccount(
+							"Admin", 
+							(Profile) (pa.findEntity("from ProfileMO where profile = 'admin'").get(0)),
+							"admin@graopara.com",
+							EJBUtility.getHash("null","MD5")
+					)
+			);
+		}
+		else user = null;
+
+		user = pa.findEntity("from User where email = 'user@graopara.com'");
+		if(user == null)
+		{
+			log.info("Criando usuário de teste...");
+			pa.saveEntity(
+					new UserAccount(
+							"Usuário Padrão", 
+							(Profile) (pa.findEntity("from ProfileMO where profile = 'user'").get(0)),
+							"user@graopara.com",
+							EJBUtility.getHash("null","MD5")
+					)
+			);
+			
+		}
+		else user = null;
+
+		user = pa.findEntity("from User where email = 'default@graopara.com'");
+		if(user == null)
+		{
+			log.info("Criando usuário sem privilégios de teste...");
+			pa.saveEntity(
+					new UserAccount(
+							"Anonimo", 
+							(Profile) (pa.findEntity("from ProfileMO where profile = 'default'").get(0)),
+							"default@graopara.com",
+							EJBUtility.getHash("null","MD5")
+					)
+			);
+		}
+		user = null;
+
+//		List<DTO> origem = pa.findEntity("from CodiceCaixaMO where tipoOrigem = 'CODICE'");
+//		if(origem == null) {
+//			log.info("Criando um origem de documento...");
+//			pa.saveEntity(new OrigemDTO("1", "CODICE", "CODICE"));
+//		}
+//		origem = pa.findEntities("from Origem where tipoOrigem = 'CAIXA'");
+//		if(origem == null) {
+//			log.info("Criando um origem de documento...");
+//			pa.saveEntity(new OrigemDTO("2", "CAIXA", "CAIXA"));
+//		}
+
+		List<DTO> typeDocument = pa.findEntity("from TipoDocumentoMO where nome = 'Ofícios'");
+		if(typeDocument == null)
+		{
+			log.info("Criando um tipo de documento...");
+			pa.saveEntity(new TipoDocumento("Ofícios", "Tipos de documentos de ofícios"));
+		}
+
+		typeDocument = pa.findEntity("from TipoDocumentoMO where nome = 'Cartas'");
+		if(typeDocument == null)
+		{
+			log.info("Criando um tipo de documento...");
+			pa.saveEntity(new TipoDocumento("Cartas", "Tipos de documentos de cartas"));
+		}
+
+		typeDocument = pa.findEntity("from TipoDocumentoMO where nome = 'Relatórios'");
+		if(typeDocument == null)
+		{
+			log.info("Criando um tipo de documento...");
+			pa.saveEntity(new TipoDocumento("Relatórios", "Tipos de documentos de relatórios"));
+		}
+		typeDocument = null;
+		
+		List<DTO> theme = pa.findEntity("from TemaPalavraChaveMO where tema = 'Autores'");
+		if(theme == null)
+		{
+			log.info("Criando um tema para palavra-chaves...");
+			pa.saveEntity(new TemaPalavraChave("Autores"));
+		}
+		
+		theme = pa.findEntity("from TemaPalavraChaveMO where tema = 'Instituição'");
+		if(theme == null)
+		{
+			log.info("Criando um tema para palavra-chaves...");
+			pa.saveEntity(new TemaPalavraChave("Instituição"));
+		}
+		
+		theme = pa.findEntity("from TemaPalavraChaveMO where tema = 'Ação'");
+		if(theme == null)
+		{
+			log.info("Criando um tema para palavra-chaves...");
+			pa.saveEntity(new TemaPalavraChave("Ação"));
+		}
+
+		log.info("Finalizando inicialização...");
+	}
+
+	private void initLogger(ServletConfig config) {
+		String logprop_config = config
+				.getInitParameter("log4j.properties-location");
+		ServletContext sc = config.getServletContext();
+
+		if (logprop_config != null) {
+			String webAppPath = sc.getRealPath("/");
+			String real_logprop = webAppPath + logprop_config;
+			File file_logprop = new File(real_logprop);
+			if (file_logprop.exists()) {
+				PropertyConfigurator.configureAndWatch(real_logprop);
+				return;
+			}
+		}
+
+		System.err
+				.println("Erro iniciando o sistema de log do arquivo de configuração");
+		System.err.println("Iniciando configuração de fallback básica...");
+		BasicConfigurator.configure();
+	}
+
+	@Override
+	public void destroy() {
+		log.info("Encerrando o sistema...");
+		log.info("Encerrando a SessionFactory do Hibernate...");
+		PersistenceUtility.closeSessionFactory();
+		log.info("Encerrando o logging...");
+		log = null;
+		//System.gc();
+		LogFactory.releaseAll();
+	}
+
+}
---
+++ GraoPara-BD2/src/main/java/webview/servlet/util/WebUtility.java
@@ -0,0 +1,289 @@
+package webview.servlet.util;
+
+import java.io.IOException;
+import java.text.Normalizer;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.jsp.JspWriter;
+
+import business.EJB.user.AuthBean;
+import business.EJB.user.UserBean;
+import business.exceptions.login.UnreachableDataBaseException;
+import business.exceptions.login.UserNotFoundException;
+
+import persistence.dto.Profile;
+
+public final class WebUtility {
+
+	public static final String cookie_email = "email_graopara";
+	public static final String cookie_session = "sessao_graopara";
+	public static final String cookie_nome = "nome_graopara";
+	public static final String cookie_status = "status_graopara";
+	public static final String[] meses = {
+		"Janeiro", "Fevereiro", "Março", "" +
+		"Abril", "Maio", "Junho", 
+		"Julho", "Agosto", "Setembro", 
+		"Outubro", "Novembro", "Dezembro"
+	}; 
+
+	public static Profile admin_profile = new Profile("admin");
+	public static Profile user_profile = new Profile("user");
+	public static Profile default_profile = new Profile("default");
+
+	public static final int cookie_expire = -1; //1 sessão dias
+
+	public static UserBean cookieLogin(Cookie[] cookie_list) {
+		if(cookie_list == null) return null;
+
+		String email = null;
+		String password = null;
+		for(int i = 0; i < cookie_list.length; i++) {
+			Cookie cookie = cookie_list[i];
+			String cName = cookie.getName();
+			if(cName.equals(WebUtility.cookie_email))
+				email = cookie.getValue();
+			if(cName.equals(WebUtility.cookie_session))
+				password = cookie.getValue();
+		}
+		if(email != null && password != null) {
+			try {
+				UserBean result = AuthBean.validarLogin(email, password, AuthBean.HashedPwd);
+				return result;
+			} catch (UnreachableDataBaseException e) {
+				e.printStackTrace();
+				return null;
+			} catch (UserNotFoundException e) {
+				return null;
+			}
+		}
+		return null;
+	}
+
+	public static Cookie selectCookie(Cookie[] c_list, String c_name) {
+		if(c_list == null) return null;
+
+		for(int i = 0; i < c_list.length; i++) {
+			Cookie cookie = c_list[i];
+			String cName = cookie.getName();
+			if(cName.equals(c_name)) return cookie;
+		}
+		return null;
+	}
+
+	public static String strDiff(String str1, String str2) {
+		int idx = str1.lastIndexOf(str2);
+	    if (idx > -1) {
+	      return str1.substring(str2.length());
+	    }
+	    else return str1;
+	}
+
+	public static String docToString(Documento doc) {
+		return
+				doc.getOrigemDocumento().getTitulo() + "\n"
+				+ doc.getAutor() + " "
+				+ doc.getDestinatario() + " "
+				+ doc.getLocal() + " "
+				+ doc.getResumo() + " "
+				+ doc.getDataDocumento().toString()
+				+ " --- "
+				+ doc.getUploader().getEmail() + " "
+				+ doc.getDataInclusao().toString()
+				+ " --- "
+				+ doc.getIdNumDocumento().getTipoId() + " "
+				+ doc.getIdNumDocumento().getCodId() + " "
+				+ doc.getOrigemDocumento().getTipoOrigem() + " "
+				+ doc.getOrigemDocumento().getCodOrigem() + " ";
+	}
+
+	public static void printName(HttpServletRequest request, JspWriter out) throws IOException {
+		Cookie name = selectCookie(request.getCookies(), WebUtility.cookie_nome);
+		if(name != null) out.write("<label>" + name.getValue() + "!</label>");
+	}
+
+	public static String printLabel(HttpServletRequest request, String parameter) throws IOException {
+		String label = request.getParameter(parameter);
+		if(label == null) return "";
+		else return label;
+	}
+
+	public static String printSelectOrigem(HttpServletRequest request) throws IOException, UnreachableDataBaseException {
+		String output = null;
+
+		OrigemDAO origemDAO = null;
+
+		try {
+			origemDAO = new OrigemDAO();
+			List<DTO> list = origemDAO.findAllOrigins();
+			List<String> aux = new ArrayList<String>();
+			output = "\n	<option selected value=\"\">Selecione...</option> ";
+
+			for(DTO d : list){
+				String tipoOrigem = ((OrigemDTO) d).getTipoOrigem();
+				if(!aux.contains(tipoOrigem)){
+					output += "\n	<option value=\"" 
+						+ tipoOrigem + " \">" 
+						+ tipoOrigem + "</option> ";
+					aux.add(tipoOrigem);
+				}
+			}
+		} catch (OriginNotFoundException e) {
+			e.printStackTrace();
+		}
+		return output;
+	}
+
+	public static String printCadastroOrigem(HttpServletRequest request) throws IOException, UnreachableDataBaseException {
+		String output = null;
+		String identificacao = request.getParameter("identificacao");
+		String codigo = request.getParameter("codigo");
+		String titulo = request.getParameter("titulo");
+
+		OrigemDAO origemDAO = null;
+
+		try {
+			origemDAO = new OrigemDAO();
+			List<DTO> list = origemDAO.findAllOrigins();
+			if(identificacao != null && codigo != null && titulo != null)
+				output = "\n	<option selected value=\"" 
+						+ identificacao + "-" + codigo + "-" + titulo + "\">" 
+						+ identificacao + " - " + codigo + " - " + titulo + "</option> ";
+
+			else	output = "\n	<option selected value=\"\">Selecione...</option> ";
+			for(DTO d : list){
+				String tipoOrigem = ((OrigemDTO) d).getTipoOrigem();
+				String codOrigem =  ((OrigemDTO) d).getCodOrigem();
+				String tituloOrigem = ((OrigemDTO) d).getTitulo();
+				if(!tipoOrigem.equals(identificacao) && !codOrigem.equals(codigo))
+					output += "\n	<option value=\"" 
+						+ tipoOrigem + "-" + codOrigem + "-" + tituloOrigem + " \">" 
+						+ tipoOrigem + " - " + codOrigem + " - " + tituloOrigem + "</option> ";
+			}
+		} catch (OriginNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		return output;
+	}
+
+	public static String printSelectId(HttpServletRequest request) throws IOException {
+		String output = null;
+		String parameter = request.getParameter("tipoAPEP_SEQ");
+		if(parameter == null)
+		{
+			output = "<option value=\"\">\"Erro\"</option>";
+		}
+		else if(parameter.equals("APEP"))
+		{
+			output = 
+					"<option selected value=\"APEP\">APEP</option> " +
+					"<option value=\"SEQ\">Sequencial</option>";
+		}
+		else if(parameter.equals("SEQ"))
+		{
+			output = 
+					"<option value=\"APEP\">APEP</option> " +
+					"<option selected value=\"SEQ\">Sequencial</option>";
+		}
+		return output;
+	}
+
+	public static String printSelectDia(HttpServletRequest request) {
+		String dia = request.getParameter("dia");
+		return "<option selected value=\"" + dia + "\">" + dia + "</option>" +
+				"<option value=\"\">--------</option>";
+	}
+
+	public static String printSelectMes(HttpServletRequest request) {
+		String mes = request.getParameter("mes");
+		int num_mes = Integer.parseInt(mes);
+		return "<option selected value=\"" + mes + "\">" + meses[num_mes - 1] + "</option>" +
+				"<option value=\"\">--------</option>";
+	}
+
+	public static String printSelectTipoDoc(HttpServletRequest request) {
+		DocumentTypeDAO dtd = new DocumentTypeDAO();
+		String result = "";
+		String tipoDoc = null;
+		try {
+			List<DTO> list = dtd.findAllDocumentTypes();
+			result = "<option value=\"\">Selecione...</option> ";
+			for(DTO d : list){
+				tipoDoc = ((TipoDocumentoDTO) d).getTipoDocumento();
+				if(tipoDoc.equals(request.getParameter("tipoDoc")))
+					result += "<option selected value=\"" + tipoDoc + "\">" + tipoDoc + "</option> ";
+				else
+					result += "<option value=\"" + tipoDoc + "\">" + tipoDoc + "</option> ";
+			}
+		} catch (UnreachableDataBaseException e) {
+			e.printStackTrace();
+		} catch (DocumentTypeNotFoundException e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+
+	public static String printInputKeyWords(HttpServletRequest request, String key_pos) {
+		BuscaPalavraChaveEJB busca = new BuscaPalavraChaveEJB();
+		String result = "";
+		String key = request.getParameter(key_pos);
+		try {
+			PalavraChaveDTO palavra = busca.buscarPalavraChave(key);
+			return "<input type=\"text\" size=\"12\" maxlength=\"32\" class=\"inputPalavraChave\" id=\""+ key_pos +"\" name=\""+ key_pos +"\" value=\"" + palavra.getPalavra() + "\"/>";
+
+		} catch (UnreachableDataBaseException e) {
+			e.printStackTrace();
+		} catch (KeywordNotFoundException e) {
+			return "<input type=\"text\" size=\"12\" maxlength=\"32\" class=\"inputPalavraChave\" id=\""+ key_pos +"\" name=\""+ key_pos +"\" value=\"\"></input> ";
+		} catch (IllegalArgumentException e){
+			return "<input type=\"text\" size=\"12\" maxlength=\"32\" class=\"inputPalavraChave\" id=\""+ key_pos +"\" name=\""+ key_pos +"\" value=\"\"></input> ";
+		}
+		return result;
+	}
+
+	public static String printSelectKeyWords(HttpServletRequest request, String key_pos) {
+		KeyWordDAO word = new KeyWordDAO();
+		String result = "";
+		String key = null;
+		try {
+			List<DTO> list = word.getAllKeys();
+
+			for(DTO d : list){
+				key = ((PalavraChaveDTO) d).getPalavra();
+				if(key.equals(request.getParameter(key_pos)))
+					result += "<option selected value=\"" + key + "\">" + key + "</option> ";
+				else
+					result += "<option value=\"" + key + "\">" + key + "</option> ";
+			}
+		} catch (UnreachableDataBaseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (KeywordNotFoundException e) {
+			// TODO Auto-generated catch block
+		}
+		return result;
+	}
+
+	public static String printOriginTitle(HttpServletRequest request) throws OriginNotFoundException {
+		OrigemDAO od = new OrigemDAO();
+		String result = "";
+		try {
+			OrigemDTO ori = od.findExactOrigin(request.getParameter("codigo"), request.getParameter("identificacao"));
+			result = ori.getTitulo().trim();
+		} catch (UnreachableDataBaseException e) {
+			e.printStackTrace();
+		} catch (OriginNotFoundException e) {
+			throw e;
+		}
+		return result;
+	}
+
+	public static String removeAccents(String str) {
+	    str = Normalizer.normalize(str, Normalizer.Form.NFD);
+	    str = str.replaceAll("[^\\p{ASCII}]", "");
+	    return str;
+	}
+}
