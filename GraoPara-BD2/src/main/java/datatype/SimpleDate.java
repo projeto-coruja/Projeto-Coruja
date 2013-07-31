d0b424eac6fa8b947e1f62d7a6538359336675f4
vitorsala <vitorsala@gmail.com>
2013-07-31 12:32:37 -0300

    e mais documentação.

--- GraoPara-BD2/src/main/java/datatype/SimpleDate.java
+++ GraoPara-BD2/src/main/java/datatype/SimpleDate.java
@@ -4,6 +4,9 @@ import java.io.Serializable;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+/**
+ * Classe para armazenamento de datas.
+ */
 public final class SimpleDate implements Serializable{
 	
 	/*
@@ -13,19 +16,38 @@ public final class SimpleDate implements Serializable{
 	public final static short TRACKS_ONLY_YEAR = 1;
 	public final static short TRACKS_YEAR_AND_MONTH = 2;
 	public final static short TRACKS_FULL_DATE = 3;
-		
+
+	/**
+	 * Construtor para data com dia, mês e ano.
+	 * @param year
+	 * @param month
+	 * @param day
+	 */
 	public SimpleDate(short year, short month, short day) {
 		this(year, month, day, TRACKS_FULL_DATE);
 	}
 	
+	/**
+	 * Construtor para data somente com mês e ano.
+	 * @param year
+	 * @param month
+	 */
 	public SimpleDate(short year, short month) {
 		this(year, month, NOT_DEFINED, TRACKS_YEAR_AND_MONTH);
 	}
 	
+	/**
+	 * Construtor para data somente com ano.
+	 * @param year
+	 */
 	public SimpleDate(short year) {
 		this(year, NOT_DEFINED, NOT_DEFINED, TRACKS_ONLY_YEAR);
 	}
 	
+	/**
+	 * Retorna a data em forma de <i>String</i>
+	 * @return - <i>String</i> contendo a data armazenada.
+	 */
 	public String format() {
 		String result = null;
 		switch (dateMode) {
@@ -47,14 +69,27 @@ public final class SimpleDate implements Serializable{
 		return result;
 	}
 	
+	/**
+	 * Retorna o tipo da data (só ano; mês e ano; dia, mês e ano)
+	 * @return dateMode
+	 */
 	public short tracks() {
 		return dateMode;
 	}
 	
+	/**
+	 * Retorna o ano.
+	 * @return year
+	 */
 	public short getYear() {
 		return year;
 	}
 	
+	/**
+	 * Retorna o mês.
+	 * @return month
+	 * @exception IllegalAccessError caso a data não esteja armazenando mês.
+	 */
 	public short getMonth() {
 		if(dateMode == TRACKS_ONLY_YEAR) {
 			throw new IllegalAccessError("Instance doesn't track month");
@@ -63,6 +98,12 @@ public final class SimpleDate implements Serializable{
 		}
 	}
 	
+
+	/**
+	 * Retorna o dia.
+	 * @return day
+	 * @exception IllegalAccessError caso a data não esteja armazenando dia.
+	 */
 	public short getDay() {
 		if(dateMode == TRACKS_ONLY_YEAR || dateMode == TRACKS_YEAR_AND_MONTH) {
 			throw new IllegalAccessError("Instance doesn't track days");
@@ -71,6 +112,12 @@ public final class SimpleDate implements Serializable{
 		}
 	}
 	
+	/**
+	 * Instncia a data a partir de uma <i>String</i>.
+	 * @param strDate - String contendo a data no formato (dd/MM/yyyy)
+	 * @return	data no formato <i>SimpleDate</i>.
+	 * @throws IllegalArgumentException
+	 */
 	public static SimpleDate parse(String strDate) throws IllegalArgumentException {
 		String[] parts = strDate.split("/");
 		if(parts.length == 1) {
@@ -97,10 +144,17 @@ public final class SimpleDate implements Serializable{
 		
 	}
 	
+	/**
+	 * 
+	 * @return
+	 */
 	public static SimpleDate getToday() {
 		return parse(sdf.format(new Date()));
 	}
 	
+	/**
+	 * @see Object#equals(Object)
+	 */
 	public boolean equals(Object anObject) {
 		if(!(anObject instanceof SimpleDate)){
 			return false;
@@ -123,6 +177,9 @@ public final class SimpleDate implements Serializable{
 		}
 	}
 	
+	/**
+	 * @return result
+	 */
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
